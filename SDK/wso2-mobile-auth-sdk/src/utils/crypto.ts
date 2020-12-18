04c95eab6138751286ea0737e8ae413d072584db
kushab <kushan.2017271@iit.ac.lk>
2020-12-19 04:45:00 +0530

    Util classes updated

--- SDK/wso2-mobile-auth-sdk/src/utils/crypto.ts
+++ SDK/wso2-mobile-auth-sdk/src/utils/crypto.ts
@@ -1,15 +1,55 @@
-import {KEYUTIL, KJUR} from 'jsrsasign';
-import { getLogger } from "log4js";
+// import {hextob64, KEYUTIL, KJUR, RSAKey} from 'jsrsasign';
+// import { getLogger } from "log4js";
+
+// export class Crypto {
+//   /**
+//    * Returns the generated keypair in PEM format
+//    */
+//   public static generateKeypair(): any {
+//     let keyPair = KEYUTIL.generateKeypair('RSA', 1024);
+//     return {
+//       pubKey: KEYUTIL.getPEM(keyPair.pubKeyObj),
+//       prvKey: KEYUTIL.getPEM(keyPair.prvKeyObj),
+//     };
+//   }
+
+//   /**
+//    * Signs the challenge and returns the signed string
+//    *
+//    * @param prvKey private key to sign the challenge
+//    * @param challenege string containing the challenge
+//    */
+//   public static signChallenge(prvKey: any, challenege: any): string {
+//     // TODO: needs to be tested on the device
+//     const logger = getLogger();
+//     logger.level = "debug";
+
+//     let sig: any = new KJUR.crypto.Signature({alg: 'SHA256withRSA'});
+//     sig.init(prvKey);
+//     sig.updateString(challenege);
+//     try {
+//       return sig.sign();
+//     } catch (err) {
+//       logger.debug('Sign Falied: ' + err);
+//     }
+//     return "Error"; // TODO: Handle this error
+//   }
+// }
+
+import {hextob64, KEYUTIL, KJUR} from 'jsrsasign';
 
 export class Crypto {
+
   /**
-   * Returns the generated keypair in PEM format
+   * Generate new keypair
+   * 
+   * @returns the generated keypair object containing PEM strings
    */
   public static generateKeypair(): any {
-    let keyPair = KEYUTIL.generateKeypair('RSA', 1024);
+    let keyPair = KEYUTIL.generateKeypair('RSA', 2048);
     return {
       pubKey: KEYUTIL.getPEM(keyPair.pubKeyObj),
-      prvKey: KEYUTIL.getPEM(keyPair.prvKeyObj),
+      prvKey: KEYUTIL.getPEM(keyPair.prvKeyObj, 'PKCS8PRV'),
     };
   }
 
@@ -18,20 +58,23 @@ export class Crypto {
    *
    * @param prvKey private key to sign the challenge
    * @param challenege string containing the challenge
+   * 
+   * @returns a base64 string of the signed challenege
    */
-  public static signChallenge(prvKey: any, challenege: any): string {
-    // TODO: needs to be tested on the device
-    const logger = getLogger();
-    logger.level = "debug";
-
-    let sig: any = new KJUR.crypto.Signature({alg: 'SHA256withRSA'});
-    sig.init(prvKey);
-    sig.updateString(challenege);
+  public static signChallenge(privateKey: string, challenge: string): string {
     try {
-      return sig.sign();
+      let prvKey = KEYUTIL.getKey(privateKey);
+      console.log("Prvkey:" + privateKey);
+      let sig: any = new KJUR.crypto.Signature({alg: 'SHA256withRSA'});
+      sig.init(prvKey);
+      // sig.updateString(challenge);
+      let signature = sig.signString(challenge);
+      return hextob64(signature);
     } catch (err) {
-      logger.debug('Sign Falied: ' + err);
+      console.log('Sign Falied: ' + err);
     }
-    return "Error"; // TODO: Handle this error
+    return "";
+
+    
   }
 }
---
+++ SDK/wso2-mobile-auth-sdk/src/utils/requestSender.ts
@@ -0,0 +1,33 @@
+import { fetch } from 'react-native-ssl-pinning';
+import { Alert } from 'react-native';
+
+
+export class RequestSender { 
+
+    public static sendRequest(url: string, requestMethod: any, requestHeaders: any, body: any) { 
+        fetch(url, {
+            method: requestMethod,
+            disableAllSecurity: true,
+            sslPinning: {
+                certs: ["wso2carbon"], // TODO: make the certificate name configurable
+            },
+            headers: requestHeaders,
+            body: body,
+            })
+            .then((response: { bodyString: string; status: number; }) => {
+            console.log(`response received ${response.bodyString}`)
+            Alert.alert(
+            "Request",
+            "Status: " + response.status + "\nMessage: " + response.bodyString,
+            [
+                { text: "OK", onPress: () => console.log("OK Pressed") }
+            ],
+            { cancelable: false }
+            );
+
+        })
+        .catch(err => {
+            console.log(`error: ${err}`)
+        });
+    }
+}
